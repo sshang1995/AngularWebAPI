90600c3eee10855e2563874a7a33a94332e5d105
sarah <sarahshang1024@gmail.com>
2020-10-26 15:58:49 -0400

    Web API working

---
+++ App_Start/WebApiConfig.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web.Http;
+using System.Web.Http.Cors;
+using Newtonsoft.Json.Serialization;
+using System.Net.Http.Headers;
+
+namespace WebAPI
+{
+    public static class WebApiConfig
+    {
+        public static void Register(HttpConfiguration config)
+        {
+            // Web API configuration and services
+
+            // Web API routes
+            config.MapHttpAttributeRoutes();
+
+            config.Routes.MapHttpRoute
+            (
+              name: "User",
+              routeTemplate: "api/users/{name}",
+              defaults: new { controller = "users", name = RouteParameter.Optional }
+            );
+
+
+            config.Routes.MapHttpRoute
+            (
+                name: "DefaultApi",
+                routeTemplate: "api/{controller}/{id}",
+                defaults: new { id = RouteParameter.Optional }
+            );
+
+            //config.Formatters.JsonFormatter.SupportedMediaTypes.Add(new MediaTypeHeaderValue("text/html"));
+
+            // Enable CORS for the Angular App
+            var cors = new EnableCorsAttribute("*", "*", "*"); /*http://localhost:4200*/
+            config.EnableCors(cors);
+
+            var jsonFormatter = config.Formatters.JsonFormatter;
+            jsonFormatter.SerializerSettings.ContractResolver = new CamelCasePropertyNamesContractResolver();
+            //config.Formatters.Remove(config.Formatters.XmlFormatter);
+            //jsonFormatter.SerializerSettings.DateTimeZoneHandling = Newtonsoft.Json.DateTimeZoneHandling.Utc;
+        }
+
+
+
+    }
+}
